80f2ab7235e52598c9b073d7f8b5e0a3cdd0a47b
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2022-05-31 15:29:30 -0400

    Update send_notify_3-0x.py

--- send_notify_3-0x.py
+++ send_notify_3-0x.py
@@ -42,10 +42,11 @@ def is_render_complete(scene):
     else:
         localizedPrint = localizedPrint[locx]
     bashCommand = f'notify-send -a "Blender" -u "critical" -i "blender" "{localizedPrint}"'
+    ps1command = 'function Show-Notification {[cmdletbinding()] Param ([string]$ToastTitle,[string][parameter(ValueFromPipeline)]$ToastText);[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > $null;$Template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02);$RawXml = [xml] $Template.GetXml();($RawXml.toast.visual.binding.text|where {$_.id -eq "1"}).AppendChild($RawXml.CreateTextNode($ToastTitle)) > $null;($RawXml.toast.visual.binding.text|where {$_.id -eq "2"}).AppendChild($RawXml.CreateTextNode($ToastText)) > $null;$SerializedXml = New-Object Windows.Data.Xml.Dom.XmlDocument;$SerializedXml.LoadXml($RawXml.OuterXml);$Toast = [Windows.UI.Notifications.ToastNotification]::new($SerializedXml);$Toast.Tag = "PowerShell";$Toast.Group = "PowerShell";$Toast.ExpirationTime = [DateTimeOffset]::Now.AddMinutes(1);$Notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier("PowerShell");$Notifier.Show($Toast)}'
     if sys.platform == "linux":
         subprocess.call(['notify-send', '-a', 'Blender', '-u', 'critical', '-i', 'blender', localizedPrint])
     elif sys.platform == "win32":
-        os.system("powershell -command \"& {$notification = New-Object -com Shell.Application;$notification.ShowBalloonTip(0, 'Blender', '" + localizedPrint + "', 'info');}\"")
+        os.system("powershell -command " + ps1command)
 classes = ()
 register, unregister = bpy.utils.register_classes_factory(classes)
 

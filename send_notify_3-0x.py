50c3aa9baa820ae66bb9619124e59c9770cddc64
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2022-05-31 15:52:18 -0400

    Update send_notify_3-0x.py

--- send_notify_3-0x.py
+++ send_notify_3-0x.py
@@ -22,53 +22,11 @@ bl_info = {
     "category": "System",
     }
 import bpy, os, locale, subprocess, sys
-if sys.platform == "win32":
-    from win32api import *
-    from win32gui import *
-    import win32con
-    import struct
-    import time
-    
-    class WindowsBalloonTip:
-        def __init__(self, title, msg):
-            message_map = {
-                    win32con.WM_DESTROY: self.OnDestroy,
-            }
-            # Register the Window class.
-            wc = WNDCLASS()
-            hinst = wc.hInstance = GetModuleHandle(None)
-            wc.lpszClassName = "PythonTaskbar"
-            wc.lpfnWndProc = message_map # could also specify a wndproc.
-            classAtom = RegisterClass(wc)
-            # Create the Window.
-            style = win32con.WS_OVERLAPPED | win32con.WS_SYSMENU
-            self.hwnd = CreateWindow( classAtom, "Taskbar", style, \
-                    0, 0, win32con.CW_USEDEFAULT, win32con.CW_USEDEFAULT, \
-                    0, 0, hinst, None)
-            UpdateWindow(self.hwnd)
-            iconPathName = os.path.abspath(os.path.join( sys.path[0], "balloontip.ico" ))
-            icon_flags = win32con.LR_LOADFROMFILE | win32con.LR_DEFAULTSIZE
-            try:
-                hicon = LoadImage(hinst, iconPathName, \
-                    win32con.IMAGE_ICON, 0, 0, icon_flags)
-            except:
-                hicon = LoadIcon(0, win32con.IDI_APPLICATION)
-            flags = NIF_ICON | NIF_MESSAGE | NIF_TIP
-            nid = (self.hwnd, 0, flags, win32con.WM_USER+20, hicon, "tooltip")
-            Shell_NotifyIcon(NIM_ADD, nid)
-            Shell_NotifyIcon(NIM_MODIFY, \
-                            (self.hwnd, 0, NIF_INFO, win32con.WM_USER+20,\
-                            hicon, "Balloon  tooltip",title,200,msg))
-            # self.show_balloon(title, msg)
-            time.sleep(10)
-            DestroyWindow(self.hwnd)
-        def OnDestroy(self, hwnd, msg, wparam, lparam):
-            nid = (self.hwnd, 0)
-            Shell_NotifyIcon(NIM_DELETE, nid)
-            PostQuitMessage(0) # Terminate the app.
-    def balloon_tip(title, msg):
-        w=WindowsBalloonTip(msg, title)
 from bpy.app.handlers import persistent
+try:
+    from plyer import notification
+except:
+    raise ImportError("win10toast is not installed. Please install it using pip install plyer")
 py_exec = sys.executable
 loc = locale.getlocale() # get current locale
 locx = loc[:3]
@@ -92,9 +50,11 @@ def is_render_complete(scene):
     if sys.platform == "linux":
         subprocess.call(['notify-send', '-a', 'Blender', '-u', 'critical', '-i', 'blender', localizedPrint])
     elif sys.platform == "win32":
-        WindowsBalloonTip(
-            title='Blender',
-            msg=localizedPrint,
+        notification.notify(
+            title="Blender",
+            message=localizedPrint,
+            app_icon="blender",
+            timeout=10
         )
 classes = ()
 register, unregister = bpy.utils.register_classes_factory(classes)

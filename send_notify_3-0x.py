4ce8b626c31e693a683e30141e5cb5199d79f409
xpsychonauticon <54873330+xpscyho@users.noreply.github.com>
2022-06-01 09:48:17 -0400

    Update send_notify_3-0x.py

--- send_notify_3-0x.py
+++ send_notify_3-0x.py
@@ -10,21 +10,21 @@
 #
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
-from datetime import datetime
 import bpy
 import os
 import locale
 import subprocess
 import sys
+from datetime import datetime
 bl_info = {
     "name": "Notify",
     "author": "xpscyho",
     "version": (1, 1),
     "blender": (3, 0, 0),
     "location": "Global",
-    "description": "Displays a system notification when render is complete on Linux and Windows based systems",
+    "description": "Displays a system notification when render is complete",
     "warning": "",
-    "wiki_url": "",
+    "wiki_url": "https://github.com/xpscyho/Notify_Blender_Render/",
     "category": "System",
 }
 py_exec = sys.executable
@@ -35,29 +35,26 @@ except:
                     "-t", os.path.join(sys.prefix, "lib", "site-packages")])
     from plyer import notification
 
-loc = locale.getlocale()  # get current locale
-locx = loc[:3]
+locx = locale.getlocale()[:3]  # get current locale
 locale.getdefaultlocale()
 
 
 @bpy.app.handlers.persistent
 def is_render_complete(scene):
-    # get render time
+    # get localization print:
     localizedPrint = {
-        "es_": "Blender | Render Finalizado!",  # Espanol
-        "ca_": "Blender | S´ha finalitzat la prestació!",  # Catalan
-        "fr_": "Blender | Rendu terminé!",  # Frances
-        "it_": "Blender | Rendering finito!",  # Italiano
-        "pt_": "Blender | Renderizado concluído!",  # Portugues
-        "de_": "Blender | Fertig machen!",  # Deutsch
+        "es_": "¡El renderizado está hecho!\n duración:",  # Espanol
+        "ca_": "el renderitzat està fet!\n durada:",  # Catalan
+        "fr_": "le rendu est fait!\n durée:",  # Frances
+        "it_": "il rendering è fatto!\n durata:",  # Italiano
+        "pt_": "renderização está feita!\n duração:",  # Portugues
+        "de_": "Das rendern ist fertig!\n Dauer:",  # Deutsch
     }
+    if not locx in localizedPrint:
+        localizedPrint = "Render is done! \n duration:" + str(datetime.now() - TIMER)
+    else:
+        localizedPrint = localizedPrint[locx] + str(datetime.now() - TIMER)
     if datetime.now - TIMER > datetime(0, 0, 0, 0, 0, 30):
-        if not locx in localizedPrint:
-            localizedPrint = "Blender | Render Finished!" + \
-                "\n" + str(datetime.now() - TIMER)
-        else:
-            localizedPrint = localizedPrint[locx] + \
-                "\n" + str(datetime.now() - TIMER)
         if sys.platform == "linux":
             subprocess.call(['notify-send', '-a', 'Blender', '-u',
                             'normal', '-i', 'blender', localizedPrint])
